3d5d90c4eca55a8de155699e4b8a3ab14d67255a
UtkarshV26 <utkarshverma2602@gmail.com>
2025-07-14 18:13:17 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score)) // here typeof is a method/function, so we can use it with or without parentheses.
+
+let numberScore = Number(score) 
+// conversion to number using Number() function
+// console.log(typeof numberScore)
+// console.log(numberScore)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// "utkarsh" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "utkarsh" => true
+// "" => false
+
+
+let newVar = 33
+
+let stringNewVar = String(newVar)
+console.log(typeof stringNewVar);
+
+console.log(stringNewVar)
