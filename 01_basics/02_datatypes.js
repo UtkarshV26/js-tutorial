ef02cf147969c1c28292e653b9390716023ddb86
UtkarshV26 <utkarshverma2602@gmail.com>
2025-07-14 14:45:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treats all JS code as newer version of JS.
+
+// alert(3+3) // we are using node.js, so alert doesn't work. It works in browser.
+console.log(3+3)
+
+console.log("work done")
+
+//Code readability is very important.
+
+let name = "utkarsh"
+let age = 22
+let isLoggedIn = true
+let projects = null
+let career;
+
+// datatypes in javascript:
+// number - 2^53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value hai ye . empty value
+// undefined => variable defined, value not given 
+// symbol => unique
+
+// object
+
+// typeof : it tells the type of variable.
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
