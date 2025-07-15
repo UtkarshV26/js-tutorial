922e68d8d3014052c8e646453616b222b40fe3d6
UtkarshV26 <utkarshverma2602@gmail.com>
2025-07-15 13:52:26 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,54 @@
+/* 
+statically typed lang
+Data types are checked at compile time. So, we need to declare type of variables.
+
+dynamically typed lang
+Data types are checked at runtime. So, we do not need to declare type to a variable. It is guessed automatically.
+
+JS is a dynamically typed language.
+let x = 10; // x is a number
+*/
+
+// Primitive : 7 types: Number, String, Boolean, Null, Undefined, Symbol, BigInt
+
+// symbol data type
+const Id = Symbol("123");
+const secondId = Symbol("123");
+
+// console.log(Id === secondId); // false, because symbols are unique
+
+// BigInt data type
+const bigNumber = 12123456789n
+
+// Reference (Non primitive) : Array, Objects, Functions
+
+const marvel = ["IronMan", "Thor", "Hulk"];
+
+let myObj = {
+    name: "Utkarsh",
+    age: 22,
+};
+
+let myFunction = function(){
+    console.log("Hello, World!");
+}
+
+
+// How to check data type in JS? using typeof operator.
+console.log(typeof secondId);
+
+/* Return datatype by typeof operator
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function(object)
+       Object  =>  object
+*/
